5158a973b905958021268a8eb152a238d4c40c6c
Beniamin Brzeziński <benek9311@wp.pl>
2023-09-20 20:46:34 +0200

    Added auth-jwt by using passport

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const bCrypt = require("bcryptjs");
 const service = require("../services/users");
 
 // Aby odpowiedzi były spójne daje przykładowy res:
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const usersController = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
 const router = express.Router();
 
@@ -7,12 +8,12 @@ router.post("/register", usersController.register);
 
 router.post("/login", usersController.login);
 
-router.post("/logout", usersController.logout);
+router.post("/logout", auth, usersController.logout);
 
 router.post("/reverify", usersController.reverifyEmail);
 
 router.get("/verify/:verificationToken", usersController.verifyEmail);
 
-router.get("/current", usersController.getCurrent);
+router.get("/current", auth, usersController.getCurrent);
 
 module.exports = router;
