ed9b465e13d8fbd085a2e6a5d55d4d290e060405
Mariusz Trocewicz <throcu@gmail.com>
2023-09-20 21:36:37 +0200

    add: models

--- src/models/transactions.js
+++ src/models/transactions.js
@@ -1,7 +1,27 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
-const transaction = new Schema({}, { versionKey: false, timestamps: true });
+const transaction = new Schema(
+  {
+    type: {
+      enum: ['-', '+'],
+    },
+    category: {
+      type: String,
+    },
+    comment: {
+      type: String,
+    },
+    sum: {
+      type: Number,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-const Transaction = model("Transaction", transaction);
+const Transaction = model('Transaction', transaction);
 
 module.exports = Transaction;
