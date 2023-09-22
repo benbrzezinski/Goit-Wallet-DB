9b285a9fe307e076b0eef037d76304797ab3cbf4
Mariusz Trocewicz <throcu@gmail.com>
2023-09-22 21:01:16 +0200

    fix: getCategory logic; response for create and update

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -35,17 +35,20 @@ const getById = async (req, res, next) => {
 const getCategory = async (req, res, next) => {
   const { id } = req.params;
   const userId = req.user._id;
-  const transaction = await service.getTransactionById(userId, id);
-  if (!transaction) {
-    return res.status(404).json({
+  try {
+    const result = await service.getTransactionCategory(userId, id);
+    if (result) {
+      return res.json({
+        status: '200',
+        statusText: 'OK',
+        data: { result },
+      });
+    }
+    res.status(404).json({
       status: '404',
       statusText: 'Not Found',
       data: `Not found transaction id: ${id}`,
     });
-  }
-  try {
-    const result = await service.getTransactionCategory(userId, id);
-    res.json(result);
   } catch (err) {
     console.error(err.message);
     next(err);
@@ -56,10 +59,11 @@ const create = async (req, res, next) => {
   const { _id } = req.user;
   const transactionData = req.body;
   try {
-    await service.createTransaction({ ...transactionData, owner: _id });
+    const result = await service.createTransaction({ ...transactionData, owner: _id });
     res.json({
-      status: 200,
-      statusText: 'OK',
+      status: 201,
+      statusText: 'Created',
+      data: { result },
     });
   } catch (err) {
     if (err.name === 'ValidationError') {
@@ -104,7 +108,7 @@ const update = async (req, res, next) => {
     res.json({
       status: 200,
       statusText: 'OK',
-      data: `Transactions ${result._id} updated`,
+      data: { result },
     });
   } catch (err) {
     if (err.name === 'ValidationError') {
